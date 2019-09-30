37133ccdec553f665868c7b940f5a29192bc5e21
zhanghqq11 <38965310+zhanghqq11@users.noreply.github.com>
2019-09-30 21:59:02 +0800

    add eslint

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,13 +1,43 @@
   import React, { Component } from 'react';
   
   class xjj extends Component {
-      state = {  }
+      constructor(props){
+        super()
+        this.state = { 
+            inputValue:"",
+            list:["基础","精油"]
+        }
+      }
       render() { 
           return (
               <div>
-                  <input /><button>增加选项</button>
+                  <input value={ this.state.inputValue} onChange={this.inputChange.bind(this)} />
+                  <button onClick={this.addList.bind(this)}>增加选项</button>
+                  <ul>
+                      {
+                          this.state.list.map((item,index)=>{
+                              return <li key={item + index}>{ item}</li>
+                          })
+                      }
+                  </ul>
               </div>
             );
+      }
+
+      //单向数据流绑定.
+      inputChange(e){
+          console.log(e.target.value)
+          this.setState({
+              inputValue: e.target.value
+          })
+      }
+
+      //改list数组
+      addList(){
+          this.setState({
+              //扩展运算符...
+              list:[...this.state.list,this.state.inputValue]
+          })
       }
   }
    
