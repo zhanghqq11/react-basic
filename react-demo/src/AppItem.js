01205c64839afd41d264914391c081a2b4f9ee0c
zhanghqq11 <zhanghqq11>
2019-10-24 09:59:39 +0800

    test

--- react-demo/src/AppItem.js
+++ react-demo/src/AppItem.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class AppItem extends Component {
     render() { 
         return (
-            <li>{this.props.content}</li>
+            <div>
+            {/* <li onClick={this.handle.click}>{this.props.content}</li> */}
+            <li>this</li>
+            </div>
           );
     }
 }
