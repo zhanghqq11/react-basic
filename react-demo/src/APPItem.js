507749657c9d409af9cb3545f518449065001e5b
zhanghqq11 <38965310+zhanghqq11@users.noreply.github.com>
2019-10-31 21:24:16 +0800

    Update APPItem.js

--- react-demo/src/APPItem.js
+++ react-demo/src/APPItem.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
 class AppItem extends Component {
-    state = {  }
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this)
+    }
+
     render() { 
-        return ( 
-            <li> Childitem</li>
-         );
+        return (
+            <li onClick={this.handleClick}>{this.props.content}</li>
+          );
+    }
+
+    handleClick() {
+       this.props.deleteItem(this.props.index);
     }
 }
- 
+
+AppItem.PropTypes={
+    content: PropTypes.string,
+    deleteItem: PropTypes.func,
+    index: PropTypes.number
+  }
+
 export default AppItem;
