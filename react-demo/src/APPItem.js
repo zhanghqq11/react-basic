289d74a9d3251d1c7a0823c46be745c8b1a9c2be
zhanghqq11 <38965310+zhanghqq11@users.noreply.github.com>
2019-10-31 21:23:21 +0800

    some

---
+++ react-demo/src/APPItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+class AppItem extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <li> Childitem</li>
+         );
+    }
+}
+ 
+export default AppItem;
