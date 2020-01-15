320ff46b08f8a86296df6fa635578bb58240bd5d
zhanghqq11 <83821904@qq.com>
2020-01-15 23:30:38 +0800

    update

--- react-demo/redux-basic/src/TodoList.js
+++ react-demo/redux-basic/src/TodoList.js
@@ -6,15 +6,24 @@ import store from './store'
 // const data = ['1hello','second hello','third hello']
 class todolist extends Component {
     constructor(props){
-        super(props);
+        super(props)
+        this.state = store.getState()
         console.log(store.getState())
+        this.changeValue = this.changeValue.bind(this)
+        this.clickAdd = this.clickAdd.bind(this)
+        this.storeChange = this.storeChange.bind(this)
+        store.subscribe(this.storeChange)
     }
     render() { 
         return (
         <div>
             <div>
-                <Input placeholder= {store.getState().inputVal} style={{width:'200px'}}/>
-                <Button>add</Button>
+                <Input 
+                placeholder= {store.getState().inputVal} 
+                style = {{width:'200px'}}
+                onChange = {this.changeValue}
+                />
+                <Button onClick={ this.clickAdd() }>add</Button>
             </div>  
             <div style={{margin:'10px',width:'300px'}}>
             <List
@@ -25,6 +34,23 @@ class todolist extends Component {
             </div>
         </div>
         );
+    }
+
+    changeValue(e){
+        const action = {
+            type: 'CHANGE_INPUT_VALUE',
+            value: e.target.value
+        }
+        store.dispatch(action)
+    }
+
+    clickAdd(){
+        console.log(111)
+        // console.log(store.getState())
+    }
+
+    storeChange(){
+        this.setState(store.getState())
     }
 }
  
--- react-demo/redux-basic/src/store/reducer.js
+++ react-demo/redux-basic/src/store/reducer.js
@@ -3,5 +3,11 @@ const defaultState = {
     list: ['get up at 4','swim 1 hour']
 }  //默认数据
 export default (state = defaultState,action)=>{  //就是一个方法函数
+    if(action.type === 'CHANGE_INPUT_VALUE'){
+        console.log(111)
+        let newState = JSON.parse(JSON.stringify(state))
+        newState.inputVal = action.value
+        return newState
+    }
     return state
 }
