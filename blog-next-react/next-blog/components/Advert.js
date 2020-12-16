726da1c2320dba011a6c4cdc6708a649bfe3d3ba
zhanghqq11 <83821904@qq.com>
2020-12-16 22:06:55 +0800

    try detail api

--- blog-next-react/next-blog/components/Advert.js
+++ blog-next-react/next-blog/components/Advert.js
@@ -6,7 +6,6 @@ import '../styles/components/Avert.css'
           <div><img src="http://blogimages.jspang.com/flutter_ad2.jpg" width="100%" /></div>
           <div><img src="http://blogimages.jspang.com/Vue_koa_ad1.jpg" width="100%" /></div>
           <div><img src="http://blogimages.jspang.com/WechatIMG12.jpeg" width="100%" /></div>
-          <div><img src="https://jspang.com/images/ad_4.jpg" width="100%" /></div>
         </div>
     )
  }
